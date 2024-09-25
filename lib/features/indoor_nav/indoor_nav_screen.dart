46a235bbbe491debc32706398c9322705e1b5d36
santosh <santoshpro95@gmail.com>
2024-09-25 16:42:22 +0530

    submit

--- lib/features/indoor_nav/indoor_nav_screen.dart
+++ lib/features/indoor_nav/indoor_nav_screen.dart
@@ -54,36 +54,12 @@ class _IndoorNavScreenState extends State<IndoorNavScreen> {
 
   // region body
   Widget body() {
-    return Column(
-      children: [
-        loadingProgress(),
-        Expanded(
-          child: StreamBuilder<bool>(
-            stream: indoorNavBloc.webLoadingCtrl.stream,
-            builder: (context, snapshot) {
-              return WebViewWidget(controller: indoorNavBloc.webViewController);
-            }
-          ),
-        ),
-      ],
-    );
-  }
-// endregion
-
-// region loadingProgress
-Widget loadingProgress(){
-    return StreamBuilder<int>(
-      stream: indoorNavBloc.progressLoadingCtrl.stream,
-      initialData: 1,
+    return StreamBuilder<bool>(
+      stream: indoorNavBloc.webLoadingCtrl.stream,
       builder: (context, snapshot) {
-        return SizedBox(
-            height: 2,
-            child: LinearProgressIndicator(color: AppColors.purpleColor,
-            backgroundColor: AppColors.greyColor,
-              value: snapshot.data!/10,
-            ));
+        return WebViewWidget(controller: indoorNavBloc.webViewController);
       }
     );
-}
+  }
 // endregion
 }
