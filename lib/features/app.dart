78b2c880cbbf29797f20b4a4d63ef9eca24bfcd7
santosh <santoshpro95@gmail.com>
2024-09-26 15:55:01 +0530

    submit

--- lib/features/app.dart
+++ lib/features/app.dart
@@ -16,6 +16,9 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light(),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData.light(),
       debugShowCheckedModeBanner: false,
       routes: {'/initial': (context) => const AvatarTTSScreen()},
       initialRoute: '/initial',
